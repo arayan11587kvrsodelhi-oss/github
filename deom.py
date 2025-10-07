8ef2b1bc89b180a61ae292337729193e93155a59
arayan11587kvrsodelhi-oss <arayan11587kvrsodelhi@gmail.com>
2025-10-07 10:50:59 +0530

    hi
    hi

---
+++ deom.py
@@ -0,0 +1,23 @@
+print("hello world")
+#this a number
+a=42
+b=9
+c=1
+s=2
+e=5
+r=6
+q=7
+n=46
+l=2
+o=8
+print("the number is",a)
+print("the number is",b)
+print("the number is",c)
+print("the number is",s)    
+print("the number is",e)
+print("the number is",r)
+print("the number is",q)
+print("the number is",n)
+print("the number is",l)
+print("the number is",o)
+
