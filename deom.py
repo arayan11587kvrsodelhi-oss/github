9ebd38de709153058922d7ce6f09e032db43b07e
arayan11587kvrsodelhi-oss <arayan11587kvrsodelhi@gmail.com>
2025-10-15 23:01:09 +0530

    hi
    hi

--- deom.py
+++ deom.py
@@ -1,23 +1,12 @@
-print("hello world")
-#this a number
-a=42
-b=9
-c=1
-s=2
-e=5
-r=6
-q=7
-n=46
-l=2
-o=8
-print("the number is",a)
-print("the number is",b)
-print("the number is",c)
-print("the number is",s)    
-print("the number is",e)
-print("the number is",r)
-print("the number is",q)
-print("the number is",n)
-print("the number is",l)
-print("the number is",o)
+#count=0
+#string="Hello, World!"
+#for i in string:
+   # if i in 'aeiouAEIOU':
+    #    count+=1
+#print("Number of vowels:", count)
 
+a="Hello, World!"
+for i in a:
+    if i in a[2]=='h':
+        print("yes")
+    else: print('no')      
